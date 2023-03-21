bfd8c146be50819d7aa295b180feb9055ee4d238
medi772425 <medi772425@gmail.com>
2023-03-21 15:13:25 +0900

    section11_118

--- 11_hooks_p1/src/050_context_file/start/components/Header.js
+++ 11_hooks_p1/src/050_context_file/start/components/Header.js
@@ -1,5 +1,28 @@
+import { useTheme } from "../context/ThemeContext";
+
 const Header = () => {
-  
+  const [theme, setTheme] = useTheme();
+  const changeTheme = (e) => setTheme(e.target.value);
+  const THEMES = ["light", "dark", "red"];
+
+  return (
+    <header className={`content-${theme}`}>
+      {THEMES.map((_theme) => {
+        return (
+          <label key={_theme}>
+            <input
+              type="radio"
+              name="radio_thema"
+              value={_theme}
+              checked={_theme === theme}
+              onChange={changeTheme}
+            />
+            {_theme}
+          </label>
+        );
+      })}
+    </header>
+  );
 };
 
 export default Header;
