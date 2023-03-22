25418343c56bdf98026801e4c8a0b20810b3b624
medi772425 <medi772425@gmail.com>
2023-03-22 21:39:12 +0900

    section11_119

--- 11_hooks_p1/src/065_useContext_with_reducer/start/components/CounterButton.js
+++ 11_hooks_p1/src/065_useContext_with_reducer/start/components/CounterButton.js
@@ -1,5 +1,22 @@
-const CounterButton = ({calcType, step, onClick}) => {
-    
-    return <button onClick={onClick}>{calcType}{step}</button>
-}
+import { useCounterDispatch } from "../context/CountContext";
+
+const CounterButton = ({ calcType, step }) => {
+  const dispatch = useCounterDispatch();
+
+  const clickHandler = () => {
+    dispatch({ type: calcType, step: step });
+
+    // TODO ここで直接呼び出すとエラーになる。 ↑のように一度、変数定義すれば問題ない
+    //      違いはなにか？
+    //      参照？しているから？
+    // useCounterDispatch({ type: calcType, step: step });
+  };
+
+  return (
+    <button onClick={clickHandler}>
+      {calcType}
+      {step}
+    </button>
+  );
+};
 export default CounterButton;
